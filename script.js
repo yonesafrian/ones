4ed1a01a157c0faea62292e645a59e53514623be
yonesafrian <yonesafryan@gmail.com>
2023-03-26 20:19:20 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+function searchMovie() {
+    $('#movie-list').html('');
+    
+    $.ajax ({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data:{
+            'apikey' : 'd589a0a4',
+            's' : $('#search-input').val()
+        },
+        success: function (result) {
+            if (result.Response == "True") {
+                let movies = result.Search;
+
+                $.each(movies, function (i, data) {
+                    $('#movie-list').append(` 
+                    <div class="col-md-4">
+                        <div class="card mb-3">
+                            <img src="` + data.Poster + `" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">`+ data.Title +`</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">`+ data.Year+`</h6>
+                                <a href="#" class="card-link" data-bs-toggle="modal" data-bs-target="#exampleModal">See Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                    `);
+                });
+                
+                $('#search-input').val('');
+
+            } else {
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">` + result.Error + `</h1>
+                    </div>
+                `)
+            }
+
+        }
+    });
+}
+
+
+$('#search-button').on('click', function() {
+    searchMovie();
+
+});
+
+$('#search-input').on('keyup', function (e) {
+    if (e.which === 13) {
+    searchMovie();
+    } 
+})
